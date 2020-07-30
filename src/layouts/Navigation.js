446c8a3e9d8a3cf9f6928475b8930f9af282fd52
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2020-07-30 13:29:22 +0200

    Add navigation styling

--- src/layouts/Navigation.js
+++ src/layouts/Navigation.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import '../scss/Navigation.scss';
 
 const links = [
-    { name: 'home', path: '/', icon: 'fas fa-home', exact: true },
-    { name: 'about', path: '/about', icon: 'fas fa-user-alt' },
-    { name: 'projects', path: '/projects', icon: 'fas fa-briefcase' },
-    { name: 'contact', path: '/contact', icon: 'fas fa-envelope' }
+    { name: 'Home', path: '/', icon: 'fas fa-home', exact: true },
+    { name: 'About', path: '/about', icon: 'fas fa-user-alt' },
+    { name: 'Projects', path: '/projects', icon: 'fas fa-briefcase' },
+    { name: 'Contact', path: '/contact', icon: 'fas fa-envelope' }
 ]
 
 const Navigation = () => {
     const item = links.map(link => (
         <li className='navigation__item' key={link.name}>
-            <NavLink to={link.path} exact={link.exact ? link.exact : false}>
+            <NavLink className='navigation__item-link' to={link.path} exact={link.exact ? link.exact : false}>
                 <span>
                     <i className={link.icon}></i>
                     <span className='navigation__item-title'>{link.name}</span>
@@ -25,13 +26,15 @@ const Navigation = () => {
                 <span className='navigation__logo-mark'>BM</span>
             </div>
             <div className='navigation__menu-btn'>
-                <div className='navigation__btn-line'></div>
-                <div className='navigation__btn-line'></div>
-                <div className='navigation__btn-line'></div>
+                <div className='navigation__menu-btn-line'></div>
+                <div className='navigation__menu-btn-line'></div>
+                <div className='navigation__menu-btn-line'></div>
             </div>
-            <ul className='navigation__items-list'>
-                {item}
-            </ul>
+            <div className='navigation__items-list-container'>
+                <ul className='navigation__items-list'>
+                    {item}
+                </ul>
+            </div>
         </div>
     )
 }
