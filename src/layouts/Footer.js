8ae8570847eddc859a734bef6bda646462a2977e
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2020-07-30 13:36:07 +0200

    Add footer structure and styling

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import '../scss/Footer.scss';
 
 const Footer = () => {
     return (
-        <div>
-            Footer
+        <div className='footer'>
+            <span>Copyright © 2020r.</span>
         </div>
     )
 }
