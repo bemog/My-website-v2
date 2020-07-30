da175132b06a0b19f99c404ade723ee98d38e2d0
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2020-07-30 11:46:50 +0200

    Base website structure

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../styles/App.css';
+import Footer from './Footer';
+import Navigation from './Navigation';
 
-function App() {
-  return (
-    <div>
-      Test
-    </div>
-  );
+class App extends Component {
+  state = {}
+  render() {
+    return (
+      <div>
+        Test
+      </div>
+    );
+  }
 }
 
 export default App;
