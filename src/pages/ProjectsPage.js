adee0fb7e9ac43ccc581a836393b7eedfbb604ea
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2020-07-30 16:27:29 +0200

    Add ProjectsPage structure and styling

--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import '../scss/ProjectsPage.scss';
+import project1 from '../images/projects/project1.png';
+import project2 from '../images/projects/project2.jpg';
+
+const projectsArray = [
+  { name: 'Project1', image: project1, alt: 'Project1 screenshot', text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, atque?' },
+  { name: 'Project2', image: project2, alt: 'Project2 screenshot', text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, atque?' },
+];
 
 const ProjectsPage = () => {
-  return (
-    <div>
-      ProjectsPage
+  const projects = projectsArray.map(project => (
+    <div className="projects__project" key={project.name}>
+      <img className='projects__project-image' src={project.image} alt={project.alt} />
+      <div className="projects__project-links">
+        <a className='projects__project_link' href="#"><span>Live</span></a>
+        <a className='projects__project_link' href="#"><span>Code</span></a>
+      </div>
+      <div className="projects__project-info">
+        <p>{project.text}</p>
+      </div>
     </div>
+  ))
+
+  return (
+    <div className='main projects'>
+      <div className='projects__projects-container'>
+        {projects}
+      </div>
+    </div >
   )
 }
 
