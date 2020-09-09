8f6f9a5c17b250357074eb57cd5b26c9c9174e9b
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2020-09-09 11:46:32 +0200

    Change welcome title

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,19 +3,23 @@ import '../scss/HomePage.scss';
 
 class HomePage extends Component {
   state = {
-    titleText: ''
-  }
+    titleText: '',
+  };
 
   render() {
     return (
-      <div className='main home'>
-        <h1 className='home__welcome-text'>
+      <div className="main home">
+        <h1 className="home__welcome-text">
           Welcome,
-        <br />
-        I am <span className='home__name mark'>Bartek Mogielnicki</span>,
-        <br />
-          <span className='home__title-text'>former mechanical constructor,</span>
-          <span className='home__title-text-end'>future web developer.</span>
+          <br />I am <span className="home__name mark">Bartek Mogielnicki</span>
+          ,
+          <br />
+          <span className="home__title-text">
+            former mechanical constructor,
+          </span>
+          <span className="home__title-text-end">
+            future Front-End Developer.
+          </span>
         </h1>
       </div>
     );
