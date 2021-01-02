003e61e46de0ff3362bb28a2a028d91494ef400d
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2021-01-02 10:29:13 +0100

    Set footer copyright date to auto adjust current year

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -2,11 +2,12 @@ import React from 'react';
 import '../scss/Footer.scss';
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
   return (
-    <div className='footer'>
-      <span>Copyright © 2020r.</span>
+    <div className="footer">
+      <span>Copyright © {currentYear}r.</span>
     </div>
-  )
-}
+  );
+};
 
 export default Footer;
