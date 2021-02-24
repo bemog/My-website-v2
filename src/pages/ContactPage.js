99232053d1d11ab6c87b27abb95c1469b2a6fe06
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2021-02-24 12:41:48 +0100

    Update linkedin account address

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -9,8 +9,8 @@ class ContactPage extends Component {
     text: '',
     nameErrorMessage: '',
     emailErrorMessage: '',
-    textErrorMessage: ''
-  }
+    textErrorMessage: '',
+  };
 
   validateForm = () => {
     let name = false;
@@ -22,7 +22,7 @@ class ContactPage extends Component {
 
     // Name input verification
     if (this.state.name === '') {
-      nameErrorMessage = 'Please enter your name'
+      nameErrorMessage = 'Please enter your name';
     } else {
       name = true;
     }
@@ -30,16 +30,16 @@ class ContactPage extends Component {
     // E-mail input verification
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (this.state.email === '') {
-      emailErrorMessage = 'Please enter your e-mail address'
-    } else if ((!re.test(String(this.state.email).toLowerCase()))) {
-      emailErrorMessage = 'E-mail address is invalid'
+      emailErrorMessage = 'Please enter your e-mail address';
+    } else if (!re.test(String(this.state.email).toLowerCase())) {
+      emailErrorMessage = 'E-mail address is invalid';
     } else {
       email = true;
     }
 
     // Text message input verification
     if (this.state.text === '') {
-      textErrorMessage = 'Please enter your message'
+      textErrorMessage = 'Please enter your message';
     } else {
       text = true;
     }
@@ -47,8 +47,8 @@ class ContactPage extends Component {
     this.setState({
       nameErrorMessage,
       emailErrorMessage,
-      textErrorMessage
-    })
+      textErrorMessage,
+    });
 
     // Submit form
     if (name && email && text) {
@@ -56,41 +56,60 @@ class ContactPage extends Component {
     } else {
       return false;
     }
-  }
+  };
 
   handleFormSubmit = (e) => {
     if (!this.validateForm()) {
       e.preventDefault();
     }
-  }
+  };
 
   handleChange = (e) => {
     const inputName = e.target.name;
     this.setState({
-      [inputName]: e.target.value
-    })
-  }
+      [inputName]: e.target.value,
+    });
+  };
 
   render() {
     return (
-      <div className='main contact'>
-        <h2 className='contact__title' >Contact</h2>
+      <div className="main contact">
+        <h2 className="contact__title">Contact</h2>
         <ContactForm
           submitted={this.handleFormSubmit}
           changed={this.handleChange}
           state={this.state}
         />
 
-        <div className='contact__info'>
-          <span><i className='contact__info-icon fas fa-user'></i>Bartek Mogielnicki</span>
-          <span><i className='contact__info-icon fas fa-phone'></i> 509-190-959</span>
-          <span><i className='contact__info-icon fas fa-envelope'></i> b.mogielnicki@gmail.com</span>
-          <div className='contact__info-social'>
-            <a href='https://www.linkedin.com/in/bartlomiej-mogielnicki/' target='_blank' rel='noopener noreferrer'>
-              <span><i className='contact__info-social-link fab fa-linkedin-in'></i></span>
+        <div className="contact__info">
+          <span>
+            <i className="contact__info-icon fas fa-user"></i>Bartek Mogielnicki
+          </span>
+          <span>
+            <i className="contact__info-icon fas fa-phone"></i> 509-190-959
+          </span>
+          <span>
+            <i className="contact__info-icon fas fa-envelope"></i>{' '}
+            b.mogielnicki@gmail.com
+          </span>
+          <div className="contact__info-social">
+            <a
+              href="https://www.linkedin.com/in/bart%C5%82omiej-mogielnicki/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <span>
+                <i className="contact__info-social-link fab fa-linkedin-in"></i>
+              </span>
             </a>
-            <a href='https://github.com/BartlomiejMogielnicki' target='_blank' rel='noopener noreferrer'>
-              <span><i className='contact__info-social-link fab fa-github'></i></span>
+            <a
+              href="https://github.com/BartlomiejMogielnicki"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <span>
+                <i className="contact__info-social-link fab fa-github"></i>
+              </span>
             </a>
           </div>
         </div>
